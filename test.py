f399103e5a073f730254397b4b8f48d9718622ac
Kipparis <keepintine@gmail.com>
2020-03-15 14:02:40 +0300

    updating database

--- test.py
+++ test.py
@@ -9,83 +9,100 @@ import argparse
 
 parser = argparse.ArgumentParser(description="write a test, store "
         "results in db, than programm will decide what words to give you")
-parser.add_argument("--db_file", type=str, default="words_res.db"
+parser.add_argument("--db_file", type=str, default="words_res.db",
         help="db filename for store results")
 parser.add_argument("-c", "--create", help="create new db",
         action="store_true")
 parser.add_argument("-w", "--words_file", type=str, default="words.dict",
         help="words file for sync with db", metavar="file_name")
 parser.add_argument("-u", "--update", help="update words in database",
-        action="store_true", default=True)
+        action="store_true", default=False)
 args = parser.parse_args()
 
 # docs here http://docs.peewee-orm.com/en/latest/peewee/models.html
 from peewee import *
 db = SqliteDatabase(args.db_file)
+
+# add logger
+import logging
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
 # about fields
 # http://docs.peewee-orm.com/en/latest/peewee/models.html#fields
-class Result(Model):
-    '''
-    table storing per-word score
-    '''
-    word = CharField()
-    score = IntegerField(default=0)
-    # default - current date
-    last_modified_date = DateField(default=datetime.now)
-    category_link = ForeignKeyField(Category, backref="results")
 
+class BaseModel(Model):
     class Meta:
         database = db   # this model uses the args.db_file database
         # change table name, default to name of class
         # table_name = string
 
-class Category(Model):
+class Category(BaseModel):
     '''
     list of existing caterogies
     '''
-    name = CharField()
+    name = CharField(default="")
 
-    class Meta:
-        database = db
 
-if args.create:
-    # create tables
-    db_cursor.execute('''CREATE TABLE categories
-            (ID int NOT NULL PRIMARY KEY,
-            category_name text
-            )''')
-    db_cursor.execute('''CREATE TABLE results
-            (ID int NOT NULL PRIMARY KEY,
-            word text,
-            score int,
-            date_added date,
-            category int DEFAULT -1
-                REFERENCES categories(ID)
-                ON UPDATE CASCADE
-                ON DELETE SET DEFAULT
-            )''')
-    # iterate over words in .dict file and uppend them
+class Result(BaseModel):
+    '''
+    table storing per-word score
+    '''
+    word = CharField(default="")
+    score = IntegerField(default=0)
+    # default - current date
+    last_modified_date = DateField(default=datetime.now)
+    category = ForeignKeyField(Category, backref="results")
+
+
+# it's good practive to explicitly open the connection
+db.connect()
+
+# this will create the tables with the appropriate columns, foreign key
+# constaints, etc ...
+db.create_tables([Result, Category])
+
+# to store models, you can use save() or create()
+
+# iterate over words in .dict file and uppend them
+if args.update:
+    previous_header = ""
+    category = None
     for entry in parse_dict(args.words_file): # must return iterable
         print(f"GOT THIIS {entry}")
         # becouse I reassign every variable for every entry
         # it gonna to be very nice to memory (but opposite to CPU)
         # for every entry -> store in db
+        if entry.category not in previous_header:
+            previous_header = entry.category
+            category = Category.create(name=previous_header)
+        # date, score applied automatically
+        Result.create(word=entry.eng_str, category=category)
 
 
-# if args.update:
+# check that all categories are in table
+# for category in Category.select():
+#     print(category.name)
 
 
 
 # TODO: in loop:
-#   1. ask user how many words he wants to learn (default = 7)
-#   2. ask user which categories does he prefer (default = all)
-#   3. run test
-#   4. collect how many words from each category does he answered,
+#   1. collect how many words from each category does he answered,
 #       output statistic
 #       on welcom page output this statistic
 #       and percantage of overall words knowledge
+#   2. ask user how many words he wants to learn (default = 7)
+#   3. ask user which categories does he prefer (default = all)
+#   4. run test
+#
 # Formalities:
 #   1. Create simple usage for newcommers
+#
+# Long term:
+#   1. Console interface
+#   2. Api
+#   3. Git support
 
 
-db_conn.close() # close connection
+db.close() # close connection
