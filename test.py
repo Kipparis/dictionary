4d3396526ab909402133e751fa6430729d7084dc
Kipparis <keepintine@gmail.com>
2020-03-27 09:45:57 +0300

    write simple update logic

--- test.py
+++ test.py
@@ -11,10 +11,11 @@ parser = argparse.ArgumentParser(description="write a test, store "
         "results in db, than programm will decide what words to give you")
 parser.add_argument("--db_file", type=str, default="words_res.db",
         help="db filename for store results")
-parser.add_argument("-c", "--create", help="create new db",
-        action="store_true")
 parser.add_argument("-w", "--words_file", type=str, default="words.dict",
         help="words file for sync with db", metavar="file_name")
+parser.add_argument("-c", "--create", help="create database (will drop"
+        " existing at specified path) (run only once)",
+        action="store_true", default=False)
 parser.add_argument("-u", "--update", help="update words in database",
         action="store_true", default=False)
 args = parser.parse_args()
@@ -24,10 +25,10 @@ from peewee import *
 db = SqliteDatabase(args.db_file)
 
 # add logger
-import logging
-logger = logging.getLogger('peewee')
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
+# import logging
+# logger = logging.getLogger('peewee')
+# logger.addHandler(logging.StreamHandler())
+# logger.setLevel(logging.DEBUG)
 
 # about fields
 # http://docs.peewee-orm.com/en/latest/peewee/models.html#fields
@@ -61,12 +62,16 @@ db.connect()
 
 # this will create the tables with the appropriate columns, foreign key
 # constaints, etc ...
-db.create_tables([Result, Category])
+# db.create_tables([Result, Category])
+if args.create:
+    print("creating new table\n=================")
+    with db:    # another way of creating tables
+        db.create_tables([Result, Category])
 
 # to store models, you can use save() or create()
 
 # iterate over words in .dict file and uppend them
-if args.update:
+if args.update or args.create:
     previous_header = ""
     category = None
     for entry in parse_dict(args.words_file): # must return iterable
@@ -74,11 +79,14 @@ if args.update:
         # becouse I reassign every variable for every entry
         # it gonna to be very nice to memory (but opposite to CPU)
         # for every entry -> store in db
-        if entry.category not in previous_header:
-            previous_header = entry.category
-            category = Category.create(name=previous_header)
+        if entry.category not in previous_header:   # category is new
+            previous_header = entry.category        # assign category to var
+            # check that category is created, if not -> create
+            category, created = Category.get_or_create(name=previous_header)
+            if not created: print(f"\"{category.name}\" already exists")
         # date, score applied automatically
-        Result.create(word=entry.eng_str, category=category)
+        result, created = Result.get_or_create(word=entry.eng_str, category=category)
+        if not created: print(f"\"{category.name}\":\"{entry.eng_str}\" - already exists")
 
 
 # check that all categories are in table
